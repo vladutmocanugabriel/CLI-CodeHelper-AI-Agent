f6fca6fadc32e6b9e5fac7afe17513216dd9b1c2
vladutmocanugabriel <mocanuvladutgabriel@gmail.com>
2025-06-26 01:24:19 +0300

    added some changes

--- calculator/main.py
+++ calculator/main.py
@@ -1,13 +1,18 @@
 # main.py
 
 import sys
+import argparse
 from pkg.calculator import Calculator
 from pkg.render import render
 
 
 def main():
+    parser = argparse.ArgumentParser(description="Evaluate a mathematical expression.")
+    parser.add_argument("expression", help="The expression to evaluate")
+    args = parser.parse_args() 
+
     calculator = Calculator()
-    expression = "3 + 7 * 2"  # Hardcoded expression
+    expression = args.expression  # Get expression from command line
     try:
         result = calculator.evaluate(expression)
         to_print = render(expression, result)
